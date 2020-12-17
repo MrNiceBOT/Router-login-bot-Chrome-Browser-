c420dae2250ef4353d2c403f178c00a753aa72e6
Taha Kara <74018704+tahakara@users.noreply.github.com>
2020-12-18 00:34:51 +0300

    Update Login_script.py

--- Login_script.py
+++ Login_script.py
@@ -1,3 +1,8 @@
+from selenium import webdriver            # pip install selenium
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 def Login():
 
     usernameStr = 'admin'
